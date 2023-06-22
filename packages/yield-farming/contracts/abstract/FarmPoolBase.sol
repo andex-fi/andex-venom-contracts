966adc8c82b05f805d4aa0bd5dbf7c4dce7d3599
Nate <89227631+natedotv2@users.noreply.github.com>
2023-06-22 13:01:48 +0100

    Added Platform.sol from broxus package

---
+++ packages/yield-farming/contracts/abstract/FarmPoolBase.sol
@@ -0,0 +1,13 @@
+pragma ever-solidity ^0.62.0;
+pragma AbiHeader expire;
+
+import "../interfaces/IUserData.sol";
+import "../interfaces/IFarmPool.sol";
+import "../interfaces/IManager.sol";
+import "./FarmPoolStorage.sol";
+import "../UserData.sol";
+
+import "../libraries/MsgFlag.sol";
+import "../Platform.sol";
+
+
