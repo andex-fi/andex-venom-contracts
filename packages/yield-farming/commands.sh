4ec42f2e9d56486731f8f71b0a34bc98698266e3
Nate <89227631+natedotv2@users.noreply.github.com>
2023-06-25 05:19:44 +0100

    Added shell command file

---
+++ packages/yield-farming/commands.sh
@@ -0,0 +1,3 @@
+locklift run -n main -s scripts/2-get-codes.ts  --config locklift.config.ts --network venom_testnet
+
+locklift run -n main -s scripts/deploy-masterchef.ts  --config locklift.config.ts --network venom_testnet
